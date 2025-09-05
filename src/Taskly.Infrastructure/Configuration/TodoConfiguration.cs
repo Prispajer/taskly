e6f7121c2baddf852f3a37e1e395fd98770e0d42
Adrian Kozieł <koziel.adrian98@gmail.com>
2025-09-06 01:25:25 +0200

    feat: add EF Core setup with Todo entity and initial migration

---
+++ src/Taskly.Infrastructure/Configuration/TodoConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Taskly.Domain.Entities;
+
+namespace Taskly.Infrastructure.Configurations;
+
+public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+{
+    public void Configure(EntityTypeBuilder<Todo> builder)
+    {
+        builder.ToTable("Todos"); builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title).HasMaxLength(50).IsRequired();
+        builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Expiry).HasColumnType("timestamp").IsRequired();
+        builder.Property(x => x.PercentComplete).HasDefaultValue(0);
+    }
+}
+
---
+++ src/Taskly.Infrastructure/Persistence/TasklyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskly.Domain.Entities;
+using Taskly.Infrastructure.Configurations;
+
+public class TasklyDbContext(DbContextOptions<TasklyDbContext> options) : DbContext(options)
+{
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        new TodoConfiguration().Configure(modelBuilder.Entity<Todo>());
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
