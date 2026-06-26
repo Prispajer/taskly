8fecab06f2c52f6bc30049cd3dc8d10688203a45
duzyk <koziel.adrian98@gmail.com>
2026-06-26 23:04:30 +0200

    feat(domain): add domain events for rich domain model

---
+++ src/Taskly.Domain/Todos/Events/SetPercentCompleteEvent.cs
@@ -0,0 +1,9 @@
+﻿using Taskly.SharedKernel.Interfaces;
+
+namespace Taskly.Domain.Todos.Events;
+
+public record SetPercentCompleteEvent(Guid TaskId) : IDomainEvent
+{
+    public Guid Id { get; init; } =  Guid.NewGuid();
+    public DateTime OccurredOn { get; init; } =  DateTime.UtcNow;
+}   
---
+++ src/Taskly.Domain/Todos/Events/TaskCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Taskly.SharedKernel.Interfaces;
+
+namespace Taskly.Domain.Todos.Events;
+
+public record TaskCreatedEvent(Guid TaskId, DateTime CreatedAt) : IDomainEvent
+{
+    public Guid Id { get; init; } =  Guid.NewGuid();
+    public DateTime OccurredOn { get; init; } =  DateTime.UtcNow;
+}   
