4a85613acc852f3da17fb77c678e9f5ee2c922e0
duzyk <koziel.adrian98@gmail.com>
2026-07-06 23:50:34 +0200

    refactor(domain): make AggregateRoot inherit from Entity base class

---
+++ src/Taskly.SharedKernel/Common/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace Taskly.SharedKernel.Common;
+
+public class Entity<TId>
+{
+    public TId Id { get; protected set; } = default!;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity<TId> entity || GetType() != entity.GetType())
+        {
+            return false;
+        }
+        
+        return EqualityComparer<TId>.Default.Equals(Id, entity.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return EqualityComparer<TId>.Default.GetHashCode(Id);
+    }
+}
