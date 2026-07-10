fc26018dd696ba5448a6acd9710d39fd345dccc5
duzyk <koziel.adrian98@gmail.com>
2026-07-10 23:59:13 +0200

    refactor(infrastructure): extract EF Core mapping for Todo entity and value objects

--- src/Taskly.Infrastructure/Configuration/TodoConfiguration.cs
+++ src/Taskly.Infrastructure/Configuration/TodoConfiguration.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Taskly.Domain.Todos;
+using Taskly.Domain.Todos.Entities;
 
-namespace Taskly.Infrastructure.Configurations;
+namespace Taskly.Infrastructure.Configuration;
 
 public class TodoConfiguration : IEntityTypeConfiguration<Todo>
 {
@@ -11,10 +11,21 @@ public class TodoConfiguration : IEntityTypeConfiguration<Todo>
         builder.ToTable("Todos"); builder.HasKey(x => x.Id);
         builder.Property(x => x.Title).HasMaxLength(50).IsRequired();
         builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
-        builder.Property(x => x.Expiry).HasColumnType("timestamp").IsRequired().HasConversion(
-        v => DateTime.SpecifyKind(v, DateTimeKind.Unspecified),
-        v => DateTime.SpecifyKind(v, DateTimeKind.Unspecified));
-        builder.Property(x => x.PercentComplete).HasDefaultValue(0);
+        builder.OwnsOne(t => t.Expiry, e =>
+        {
+            e.Property(x => x.Value)
+                .HasColumnName("Expiry")
+                .HasColumnType("timestamp")
+                .IsRequired();
+        });
+        builder.OwnsOne(t => t.PercentComplete, pc =>
+        {
+            pc.Property(x => x.Value)
+                .HasColumnName("PercentComplete")
+                .HasColumnType("integer")
+                .HasDefaultValue(0);
+        });
+        builder.Ignore(x => x.DomainEvents);
     }
 }
 
