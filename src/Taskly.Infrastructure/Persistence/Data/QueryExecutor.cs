af0175872a20b0d4b6d16bddd60a5bc9184b85f1
duzyk <koziel.adrian98@gmail.com>
2026-07-23 23:27:38 +0200

    feat(infrastructure): implement TodoRepository and QueryExecutor for CQRS pattern

---
+++ src/Taskly.Infrastructure/Persistence/Data/QueryExecutor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskly.Application.Abstractions.Data;
+
+namespace Taskly.Infrastructure.Persistence.Data;
+
+public sealed class QueryExecutor : IQueryExecutor {
+    public async Task<List<T>> ExecuteListAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) => await query.ToListAsync(cancellationToken);
+    public async Task<T?> ExecuteSingleOrDefaultAsync<T>(IQueryable<T> query, CancellationToken cancellationToken) => await query.SingleOrDefaultAsync(cancellationToken);
+} 
---
+++ src/Taskly.Infrastructure/Persistence/Repositories/TodoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Taskly.Application.Abstractions.Data;
+using Taskly.Domain.Todos.Entities;
+
+namespace Taskly.Infrastructure.Persistence.Repositories;
+
+public class TodoRepository(TasklyDbContext context) : ITodoRepository
+{
+    public IQueryable<Todo> GetQueryable() => context.Todos;
+    public void Add(Todo todo) => context.Add(todo);
+    public void Remove(Todo todo) => context.Remove(todo);
+}   
